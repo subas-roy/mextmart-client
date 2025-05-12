9f896e7b90c295eb828fa7571668b72dcb0ba635
subas-roy <subasroy46@gmail.com>
2025-05-12 07:28:57 +0600

    Create route groups & setup header and footer

--- src/app/(WithCommonLayout)/page.tsx
+++ src/app/(WithCommonLayout)/page.tsx
@@ -3,6 +3,7 @@ import { Button } from "@/components/ui/button";
 const HomePage = () => {
   return (
     <div>
+      <h1>Welcome to NextMart</h1>
       <Button>Click Me</Button>
     </div>
   );
